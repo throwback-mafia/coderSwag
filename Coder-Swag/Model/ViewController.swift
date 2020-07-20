7a5a650c8b4b8b0db12fc55aef13a437c5cf61d1
Jithin Abhishek <jithinabhishek@gmail.com>
2020-07-20 17:04:15 +0530

    Second ios project

---
+++ Coder-Swag/Model/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  Coder-Swag
+//
+//  Created by jithin abhishek on 19/07/20.
+//  Copyright © 2020 jithin abhishek. All rights reserved.
+//
+
+import UIKit
+
+class CategoriesVC: UIViewController {
+    @IBOutlet weak var categoryTable:UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
